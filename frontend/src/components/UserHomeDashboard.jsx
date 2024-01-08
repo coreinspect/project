e6eac5b7d8138a5ab97304263ad1ac7337ae8873
Jury Gregorio <jury.romblondev@outlook.com>
2024-01-08 08:21:13 +0800

    Updated CSS and HTML

--- frontend/src/components/UserHomeDashboard.jsx
+++ frontend/src/components/UserHomeDashboard.jsx
@@ -9,8 +9,22 @@ const UserHomeDashboard = () => {
         </div>
         <div className="user-home-details">
           <div className="user-home-info">
-            <h3>Welcome to CoderDiv John</h3>
-            <p>Software Engineer</p>
+            <h3 className="user-home-name">Welcome Jury!</h3>
+            <p className="user-home-status">Verified</p>
+            <p>
+              Clean code always looks like it was written by someone who cares.
+            </p>
+          </div>
+          <div className="user-home-links">
+            <div className="user-home-btn">
+              <button>Add Post</button>
+              <button>Profile</button>
+              <button>Sign Out</button>
+            </div>
+
+            <div className="user-home-input">
+              <input type="text" placeholder="Search" />
+            </div>
           </div>
         </div>
       </div>
