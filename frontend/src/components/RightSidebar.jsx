2b9e3fbe1de6951897948adc60c3b1409ae937f5
Jury Gregorio <jury.devs@gmail.com>
2024-01-08 17:23:42 +0800

    Update CSS

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -1,6 +1,28 @@
-import './css/rightsidebar.css';
+import "./css/rightsidebar.css";
 const RightSidebar = () => {
-  return <div className="rightcontainer">RightSidebar</div>;
+   return (
+      <div className="rightcontainer">
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim fugit,
+            voluptatem quod officia sint vero nulla dolores corporis eos saepe
+            aut minus ex fugiat odio nam adipisci autem eveniet natus? Est
+            ipsum, fugiat alias dolor magni quasi reprehenderit quos numquam
+            sequi fugit quod, a magnam iure expedita accusamus animi inventore
+            quam nostrum cum amet! Aliquam aliquid odit numquam repudiandae
+            beatae? Neque animi quia, suscipit ab reprehenderit perferendis
+            nesciunt voluptatibus magnam rem earum quasi odit fugiat veniam nam
+            expedita. Dolorum fugit amet a, repellendus veritatis ipsam iure
+            odit voluptatem? Officiis, dolorum. Quaerat tempora incidunt nisi
+            veritatis quia nihil, soluta debitis voluptas vitae impedit, sit
+            exercitationem ab molestiae suscipit laboriosam voluptatibus, id
+            quibusdam odio aspernatur sapiente eius? Necessitatibus ipsa illo
+            aliquam reprehenderit. Commodi corrupti ipsum, dolorum eius quas
+            nostrum eos ad sed veritatis ratione itaque, soluta earum, repellat
+            sapiente praesentium aliquam! Molestias sed totam quibusdam sapiente
+            magni optio aut alias, inventore adipisci.
+         </p>
+      </div>
+   );
 };
 
 export default RightSidebar;
--- frontend/src/components/UserHomeDashboard.jsx
+++ frontend/src/components/UserHomeDashboard.jsx
@@ -1,35 +1,44 @@
-import './css/userhomedashboard.css';
-import { images } from '../constants';
+import "./css/userhomedashboard.css";
+import { images } from "../constants";
 const UserHomeDashboard = () => {
-  return (
-    <div className="user-home-dashboard">
-      <div className="user-home-container">
-        <div className="user-home-profile">
-          <img src={images.NoProfilePicture} alt="" />
-        </div>
-        <div className="user-home-details">
-          <div className="user-home-info">
-            <h3 className="user-home-name">Welcome Jury!</h3>
-            <p className="user-home-status">Verified</p>
-            <p>
-              Clean code always looks like it was written by someone who cares.
-            </p>
-          </div>
-          <div className="user-home-links">
-            <div className="user-home-btn">
-              <button>Add Post</button>
-              <button>Profile</button>
-              <button>Sign Out</button>
+   return (
+      <div className="user-home-dashboard">
+         <div className="user-home-container">
+            <div className="user-home-profile">
+               <img src={images.NoProfilePicture} alt="" />
             </div>
+            <div className="user-home-details">
+               <div className="user-home-info">
+                  <h3 className="user-home-name">Welcome Jury!</h3>
+                  <p className="user-home-status">Verified</p>
+                  <p className="user-home-quote">
+                     Clean code always looks like it was written by someone who
+                     cares.
+                  </p>
+               </div>
+               <div className="user-home-links">
+                  <div className="user-home-btn">
+                     <button className="user-home-add-post">Add Post</button>
+                     <button className="user-home-dashprofile">Profile</button>
+                     <button className="user-home-out">Sign Out</button>
+                  </div>
 
-            <div className="user-home-input">
-              <input type="text" placeholder="Search" />
+                  <div className="user-home-input-dash">
+                     <div className="user-home-input-container">
+                        <input
+                           placeholder="Search.."
+                           id="input"
+                           className="user-home-input"
+                           name="text"
+                           type="text"
+                        />
+                     </div>
+                  </div>
+               </div>
             </div>
-          </div>
-        </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default UserHomeDashboard;
